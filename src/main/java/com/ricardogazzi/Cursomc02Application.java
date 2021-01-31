33208fb93b6e05c4d7514d59f1b71cb3110b38a9
ricardogazzi <ricardogazzi2@gmail.com>
2021-01-31 19:49:05 -0300

    projeto criado

---
+++ src/main/java/com/ricardogazzi/Cursomc02Application.java
@@ -0,0 +1,13 @@
+package com.ricardogazzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardogazzi/Cursomc02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardogazzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomc02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
