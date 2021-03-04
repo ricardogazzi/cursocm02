9177c47907c04143ecd927ff5bc0ede2ed9237bd
ricardogazzi <ricardogazzi2@gmail.com>
2021-03-04 10:53:51 -0300

    Criando operação de instanciação

--- src/main/java/com/ricardogazzi/Cursomc02Application.java
+++ src/main/java/com/ricardogazzi/Cursomc02Application.java
@@ -1,13 +1,33 @@
 package com.ricardogazzi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Cursomc02Application {
+import com.ricardogazzi.domain.Categoria;
+import com.ricardogazzi.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class Cursomc02Application implements CommandLineRunner {//Implem. met. auxiliar p/ executar ação qdo aplicação iniciar
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; //Puxa cat1 e 2 para BD
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc02Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Categoria cat1 = new Categoria(null, "Informática"); //Instancia das categorias. (ID = null porque é gerado autom.)
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));//Salvando no BD.
+		
 	}
 
 }
