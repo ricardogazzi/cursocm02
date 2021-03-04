e71ec714582c47261dd3a143dbd8ecc315be6d01
ricardogazzi <ricardogazzi2@gmail.com>
2021-03-04 09:44:30 -0300

    Criando repository e service para Categoria

--- src/main/java/com/ricardogazzi/resources/CategoriaResource.java
+++ src/main/java/com/ricardogazzi/resources/CategoriaResource.java
@@ -3,20 +3,31 @@ package com.ricardogazzi.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ricardogazzi.domain.Categoria;
+import com.ricardogazzi.services.CategoriaService;
 
 @RestController // Adicionar e importar
 @RequestMapping(value = "/categorias") // Criação de classe que é controlador rest e responde pelo endpoint categorias
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET) // função rest se associa a verbo http
-	public List<Categoria> listar() { // mudar String para List<Categoria>
-
-		Categoria cat1 = new Categoria(1, "Informática"); //  Instanciação dos objetos tipo categoria
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value =  "/{id}", method = RequestMethod.GET) // função rest se associa a verbo http
+	public ResponseEntity<?> find(@PathVariable Integer id) { // mudar String para List<Categoria>
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
+		
+		/*Categoria cat1 = new Categoria(1, "Informática"); //  Instanciação dos objetos tipo categoria
 		Categoria cat2 = new Categoria(2, "Escritório");
 
 		List<Categoria> lista = new ArrayList<>(); // lista de categorias. Importar e etc.
@@ -24,6 +35,6 @@ public class CategoriaResource {
 		lista.add(cat2);
 
 		return lista; // Mudar frase para lista. Retorna lista de categorias.
-	}
+	}*/
 
-}
+
---
+++ src/main/java/com/ricardogazzi/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.ricardogazzi.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ricardogazzi.domain.Categoria;
+import com.ricardogazzi.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		} 
+
+}
